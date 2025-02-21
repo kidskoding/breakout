9cdf8696815be2f07ee18308f4bccd4700eb4163
Anirudh Konidala <kidskodingclub@gmail.com>
2025-02-21 06:08:50 -0600

    added a basic pygame environment

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello, World!')
+import pygame
+
+pygame.init()
+
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Breakout")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
